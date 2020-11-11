0e239e5591cf5194c198a87c1061081b8fae0f4e
Minazuddin <minazuddin23@gmail.com>
2020-11-11 11:42:28 +0530

    removed unused var

--- services/mail.service.js
+++ services/mail.service.js
@@ -1,10 +1,6 @@
 const nodemailer = require('nodemailer');
 
 exports.sendMail = (options) => {
-    const   host = process.env.HOST,
-            port = process.env.PORT,
-            username = process.env.USERNAME,
-            password = process.env.PASSWORD
     const   transport = nodemailer.createTransport({
             host: 'smtp.gmail.com',
             port: '587',
